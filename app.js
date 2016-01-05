a018f8c053421d060eee880d79aa351cce35a15b
Jamie Kelly <jamie@studio174.ca>
2016-01-05 15:36:34 -0800

    adds passport module for authentication

--- app.js
+++ app.js
@@ -1,60 +1,104 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/*
+ * express app for PLAR
+ */
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// express app dependencies
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
 
-var app = express();
+    // for user authentication
+    hash = require('bcrypt-nodejs'),
+    passport = require('passport'),
+    localStrategy = require('passport-local').Strategy,
 
-// view engine setup
+    // our express app
+    app = express(),
+
+    // require our user model
+    User = require('./models/user.js');
+
+// require mongolab database connection
+require('./lib/connection');
+
+// tell the application to use JADE as its templating engine when using .render
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+/*
+ * define our middlewares 
+ */
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.use('/', routes); 
-app.use('/users', users);
+// for session managment
+app.use(require('express-session')({
+    secret: 'flyinh spaghetti monster',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// passport auth middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// path to app assetts 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// configure passport
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// allows the server to serve up injected bower dependencies
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+// application routes
+app.use('/', require('./routes/index.js'));
+app.use('/user/', require('./routes/api.js'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
 
+/*
+ * error handling
+ */
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
