37f407a5e5c7135bf1671383a25aa94daaf7fa25
Jamie Kelly <jamie@studio174.ca>
2016-01-07 20:19:15 -0800

    updates comments

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,32 @@ var express = require('express'),
     passport = require('passport');
 User = require('../models/user.js');
 
+// 
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+
+        if (!user) {
+            return res.status(200).json({
+                err: info
+            });
+        }
+
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(500).json({
+                    err: 'Could not log in user'
+                });
+            }
+            res.status(200).json({
+                status: 'Login successful!'
+            });
+        });
+
+    })(req, res, next);
+});
 
 router.post('/register', function(req, res) {
     User.register(new User({
@@ -21,28 +47,7 @@ router.post('/register', function(req, res) {
     });
 });
 
-router.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({
-                err: info
-            });
-        }
-        req.logIn(user, function(err) {
-            if (err) {
-                return res.status(500).json({
-                    err: 'Could not log in user'
-                });
-            }
-            res.status(200).json({
-                status: 'Login successful!'
-            });
-        });
-    })(req, res, next);
-});
+
 
 router.get('/logout', function(req, res) {
     req.logout();
