6a28782c5dd93b8435ad5a7327618de435725241
Jamie Kelly <jamie@studio174.ca>
2016-01-07 20:19:01 -0800

    adds login controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,29 +1,49 @@
-angular.module('myApp').controller('loginController', ['$scope', '$location', 'AuthService',
+angular.module('myApp').controller('AppCtrl', ['$scope', '$location',
+    function($scope, $location) {
+        $scope.appState = 'loggedIn';
+        console.log($scope.hello);
+    }
+]);
+
+angular.module('myApp').controller('LoginCtrl', ['$scope', '$location', 'AuthService',
+
     function($scope, $location, AuthService) {
 
-        console.log(AuthService.getUserStatus());
+        // console.log(AuthService.getUserStatus());
+        $scope.myValue = true;
 
         $scope.login = function() {
 
-            // initial values
-            $scope.error = false;
-            $scope.disabled = true;
 
-            // call login from service
-            AuthService.login($scope.loginForm.username, $scope.loginForm.password)
-                // handle success
-                .then(function() {
-                    $location.path('/');
-                    $scope.disabled = false;
-                    $scope.loginForm = {};
-                })
-                // handle error
-                .catch(function() {
-                    $scope.error = true;
-                    $scope.errorMessage = "Invalid username and/or password";
-                    $scope.disabled = false;
-                    $scope.loginForm = {};
-                });
+            if ($scope.loginForm === undefined) {
+
+                // return an error when the user trys to submit the form without entering credentials
+                $scope.error = true;
+                $scope.errorMessage = "Invalid username or password, please try again";
+
+            } else {
+
+                // no error
+                $scope.error = false;
+                $scope.disabled = true;
+
+                // call login from service
+                AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+                    // handle success
+                    .then(function() {
+                        $location.path('/');
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    })
+                    // handle error
+                    .catch(function() {
+                        $scope.error = true;
+                        $scope.errorMessage = "Invalid username or password, please try again";
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    });
+            }
+
 
         };
 
@@ -31,7 +51,7 @@ angular.module('myApp').controller('loginController', ['$scope', '$location', 'A
 ]);
 
 
-angular.module('myApp').controller('logoutController', ['$scope', '$location', 'AuthService',
+angular.module('myApp').controller('LogoutCtrl', ['$scope', '$location', 'AuthService',
     function($scope, $location, AuthService) {
 
         $scope.logout = function() {
